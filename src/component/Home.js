55421db532da152aa607f763a06928e2b916cf83
alexlucas <alex.plomberie83@gmail.com>
2019-04-13 21:03:46 +0200

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bienvenue sur l'accueil</h1>
+      </div>
+    )
+  }
+}
